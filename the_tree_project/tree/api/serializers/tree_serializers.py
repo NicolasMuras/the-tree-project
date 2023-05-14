6b7bbf1d81bf4f06c748aa5a6cfc6388dd5c0a1a
LucianaAG <lucygauto35@gmail.com>
2023-05-14 18:33:50 -0300

    feat: generar rectangulo

--- the_tree_project/tree/api/serializers/tree_serializers.py
+++ the_tree_project/tree/api/serializers/tree_serializers.py
@@ -3,6 +3,7 @@ from tree.models import Square, Triangle, Coordinate
 
 
 class CoordinateSerializer(serializers.ModelSerializer):
+
     class Meta:
         model = Coordinate
         fields = '__all__'
@@ -14,6 +15,7 @@ class CoordinateSerializer(serializers.ModelSerializer):
         }
 
 class TriangleSerializer(serializers.ModelSerializer):
+
     coordinates = CoordinateSerializer(many=True)
 
     class Meta:
@@ -30,4 +32,26 @@ class SquareSerializer(serializers.ModelSerializer):
 
     def get_triangles(self, instance):
         return list(instance.triangles.values())
+    
+    def create(self, validated_data):
+        triangles_data = validated_data.pop('triangles', [])
+        id_custom = validated_data.pop('id_custom', 0)
+        print(id_custom)
+        if id != 0:
+            square = Square.objects.create(id=id_custom, **validated_data)
+        else:
+            square = Square.objects.create(**validated_data)
+        triangle_array = []
+        for triangle_data in triangles_data:
+            coordinates_data = triangle_data.pop('coordinates', [])
+            triangle = Triangle.objects.create(square=square, **triangle_data)
+            triangle_array.append(triangle)
+            coordinate_array = []
+            for coordinate_data in coordinates_data:
+                coordinate = Coordinate.objects.create(triangle=triangle, **coordinate_data)
+                coordinate_array.append(coordinate)
+            triangle.coordinates.set(coordinate_array)
+        square.triangles.set(triangle_array)
+        square.save()
+        return square
 
--- the_tree_project/tree/apps.py
+++ the_tree_project/tree/apps.py
@@ -1,5 +1,5 @@
 from django.apps import AppConfig
 
 
-class SquareConfig(AppConfig):
-    name = 'Square'
+class TreeConfig(AppConfig):
+    name = 'tree'
